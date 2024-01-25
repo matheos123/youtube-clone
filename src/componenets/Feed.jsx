1106b22b05a6c38a1d1de662cc4c2169215d7c1c
matheos123 <matheosbelay@gmail.com>
2024-01-25 13:35:19 +0300

    final finished code

--- src/componenets/Feed.jsx
+++ src/componenets/Feed.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
+import Videos from "./Videos";
 import Sidebar from "./Sidebar";
+import { fetchFromAPi } from "../utils/fetchFromApi";
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    setVideos(null);
+    fetchFromAPi(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]); //only run when the page reloads
+
   return (
     <Stack
-      sx={{
-        flexDirection: { sx: "column", md: "row" },
-      }}
+    sx={{ flexDirection: { sx: "column", md: "row" } }}
     >
       <Box
         sx={{
@@ -16,7 +25,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -24,6 +36,20 @@ const Feed = () => {
         >
           Copyright 2024 matheos belay
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto ", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          {selectedCategory}
+          <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
